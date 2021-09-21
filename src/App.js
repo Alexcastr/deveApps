fe46c3769d25fd8d3450f5436ce986dac77e4993
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-09-21 16:27:22 -0500

    Integrar la interfaz login con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header></header>
+    <main>
+      <section>
+        <form className="contenedorLogin">
+          <h1 className="tituloForm">Iniciar Sesión</h1>
+          <div className="datosFormulario">
+            <div className="seccionDatos">
+              <div>
+                <label for="usuario">
+                  <span className="etiqueta">Identificación</span>
+                </label>
+              </div>
+              <div className="campo">
+                <input
+                  name="usuario"
+                  type="text"
+                  required
+                  className="campoEntrada"
+                />
+              </div>
+              <br />
+            </div>
+
+            <div className="seccionDatos">
+              <div>
+                <label for="contraseña">
+                  <span className="etiqueta">Contraseña</span>
+                </label>
+              </div>
+              <div className="campo">
+                <input type="password" required className="campoEntrada" />
+              </div>
+              <br />
+            </div>
+
+            <div className="seccionDatos">
+              <div>
+                <label for="rol">
+                  <span className="etiqueta">Rol</span>
+                </label>
+              </div>
+              <div className="campo">
+                <select name="rol" className="campoEntrada">
+                  <option value="rol1">Vendedor</option>
+                  <option value="rol2">Administrador</option>
+                  <option value="rol3">Ejecutivo</option>
+                  <option value="rol4">Operario</option>
+                  <option value="rol5">Director</option>
+                  <option value="rol6">Gerente Comercial</option>
+                </select>
+              </div>
+              <br />
+            </div>
+          </div>
+          <div className="botonCaja">
+            <button type="submit" className="boton">Ingresar</button>
+            <button type="reset" className="boton">Limpiar</button>
+          </div>
+        </form>
+      </section>
+    </main>
     </div>
   );
 }
