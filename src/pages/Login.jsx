d8e45b255031bd78417ac27cb1ad581cd3502a70
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-09-28 14:46:40 -0500

    cambios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
       <main>
-        <section className="contenedorPrincipal">
-          <form className="contenedorLogin">
-            <h1 className="tituloForm">DEVEAPP'S</h1>
-            <div className="datosFormulario">
-              <div className="seccionDatos">
+        <section className="mainContainer">
+          <form className="loginContainer">
+            <h1 className="formTittle">DEVEAPP'S</h1>
+            <div className="formField">
+              <div className="dataSection">
                 <div>
                   <label for="correo">
-                    <span className="etiqueta">Correo</span>
+                    <span className="label">Correo</span>
                   </label>
                 </div>
-                <div className="campo">
+                <div className="field">
                   <input
                     name="correo"
                     type="email"
                     required
-                    className="campoEntrada"
+                    className="entry"
                   />
                 </div>
-                {/* <br /> */}
-              </div>
+               </div>
 
-              <div className="seccionDatos">
+              <div className="dataSection">
                 <div>
                   <label for="contraseña">
-                    <span className="etiqueta">Contraseña</span>
+                    <span className="label">Contraseña</span>
                   </label>
                 </div>
-                <div className="campo">
-                  <input type="password" required className="campoEntrada" />
+                <div className="field">
+                  <input type="password" required className="entry" />
                 </div>
-                {/* <br /> */}
               </div>
             </div>
-            <div className="botonCaja">
-              <button type="submit" className="boton">
-                LOG IN
+            <div className="buttonField">
+              <Link to="/usuarios" className="loginButton">
+              <button type="submit">
+                LOGIN
               </button>
-              <span className="etiqueta crearCuenta">¿No tienes una cuenta?</span>
+              </Link>
+              <span className="label newAccount">¿No tienes una cuenta?</span>
             </div>
           </form>
         </section>
