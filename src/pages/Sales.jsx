02d46d4c2027ac30c3decdf83e6bef48aa855df9
Cristian <krcamilo0417@gmail.com>
2021-09-28 17:41:57 -0500

    Pagina de redireccionamiento para admins

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Header from 'Components/header'
+import Header from "Components/header";
 const Sales = () => {
-    return (
-        <>
-         <Header current="ventas"/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header current="ventas" />
+    </>
+  );
+};
 
-export default Sales
+export default Sales;
