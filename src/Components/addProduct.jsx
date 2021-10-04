c7e05a9adf4688354d1341d939c21c0122e088bc
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-03 20:47:15 -0500

    organizar formulario añadir productos

--- src/Components/addProduct.jsx
+++ src/Components/addProduct.jsx
@@ -20,43 +20,46 @@ const AddProduct = () => {
   };
   return (
     <>
-      <body className="center-content mt-1">
-        <div className="table">
-          <div className="BodyAddProduct">
-            <div className="iconAddProduct">
-              <i class="bi bi-server navButton"></i>
-            </div>
-            <div  className="gridAddProduct">
-              <form ref={form} onSubmit={submitForm} className="productForm">
-                <div className="item1">
-                  <label>
-                    ID:
-                    <input type="number" name="id" required />
-                  </label>
-                  <label>
-                    Estado:
-                    <select className="item1-select"placeholder="seleccionar" name="available" required>
-                      <option>Disponible</option>
-                      <option>No Disponible</option>
-                    </select>
-                  </label>
-                </div>
-                <label className="item2">
-                  Valor unitario:
-                  <input type="number" name="value" required />
-                </label>
-                <label className="item3">
-                  Descripción:
-                  <textarea rows="5" cols="80" name="description"></textarea>
-                </label>
-                <div className="buttonAddProduct">
-                  <button type="submit">Guardar</button>
-                </div>
-              </form>
-            </div>
+      <body className="FormVenta">
+        <div className="BodyAddProduct">
+          <div className="iconAddProduct">
+            <i class="bi bi-server navButton"></i>
           </div>
-          <ToastContainer position="bottom-center" autoClose={5000} />
+          <div className="gridAddProduct">
+            <form ref={form} onSubmit={submitForm} className="productForm">
+              <div className="item1">
+                <label>
+                  ID:
+                  <input type="number" name="id" required />
+                </label>
+                <label>
+                  Estado:
+                  <select
+                    className="item1-select"
+                    placeholder="seleccionar"
+                    name="available"
+                    required
+                  >
+                    <option>Disponible</option>
+                    <option>No Disponible</option>
+                  </select>
+                </label>
+              </div>
+              <label className="item2">
+                Valor unitario:
+                <input type="number" name="value" required />
+              </label>
+              <label className="item3">
+                Descripción:
+                <textarea rows="5" cols="80" name="description"></textarea>
+              </label>
+              <div className="buttonAddProduct">
+                <button type="submit">Guardar</button>
+              </div>
+            </form>
+          </div>
         </div>
+        <ToastContainer position="bottom-center" autoClose={5000} />
       </body>
     </>
   );
