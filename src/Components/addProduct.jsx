5bd2cac99c0da55f63bea73f676c916b83260e40
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-03 20:19:36 -0500

    Organizar formulario añadir producto

--- src/Components/addProduct.jsx
+++ src/Components/addProduct.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { useRef} from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AddProduct = () => {
-  
   // const [mostrarProductos,setMostrarProductos] = useState(false);
 
   const form = useRef(null);
 
-  const submitForm = (e) =>{
+  const submitForm = (e) => {
     e.preventDefault();
     const fd = new FormData(form.current);
 
@@ -17,58 +16,49 @@ const AddProduct = () => {
     fd.forEach((value, key) => {
       nuevoProducto[key] = value;
     });
-    toast.success('Vehículo agregado con éxito');
-  }; 
+    toast.success("Vehículo agregado con éxito");
+  };
   return (
-      <>
+    <>
       <body className="center-content mt-1">
-       <div className="table">
-        <div className="BodyAddProduct">
-          <div className="iconAddProduct">
-            <i class="bi bi-server navButton"></i>
-                </div>
-                <div className="gridAddProduct">
-                <form ref={form} onSubmit={submitForm}>
-                    <div className="item1">
-                    <div>
-                    <label>
-                    ID:
-                    <input type="number" name = "id" required/>
-                    </label>
-                    </div>
-                    <div>
-                    <label>
-                    Estado:
-                    <select placeholder="seleccionar" name="available" required>
-                        <option>Disponible</option>
-                        <option>No Disponible</option>
-                    </select>
-                    </label>
-                    </div>
-                    </div>
-                    <div className="item3">
-                    <label>
-                    Valor unitario:
-                    <input type="number" name="value" required/>
-                    </label>
-                    </div>
-                    <div className="item4">
-                    <label>
-                    Descripción:
-                    <br/>
-                    <textarea rows="5" cols="80" name="description"></textarea>
-                    </label>
-                    </div>
-                    <div>
-                    <button type="submit">Guardar</button>
-                    </div>
-                </form>
-                </div>
+        <div className="table">
+          <div className="BodyAddProduct">
+            <div className="iconAddProduct">
+              <i class="bi bi-server navButton"></i>
             </div>
-            <ToastContainer position='bottom-center' autoClose={5000} />
+            <div  className="gridAddProduct">
+              <form ref={form} onSubmit={submitForm} className="productForm">
+                <div className="item1">
+                  <label>
+                    ID:
+                    <input type="number" name="id" required />
+                  </label>
+                  <label>
+                    Estado:
+                    <select className="item1-select"placeholder="seleccionar" name="available" required>
+                      <option>Disponible</option>
+                      <option>No Disponible</option>
+                    </select>
+                  </label>
+                </div>
+                <label className="item2">
+                  Valor unitario:
+                  <input type="number" name="value" required />
+                </label>
+                <label className="item3">
+                  Descripción:
+                  <textarea rows="5" cols="80" name="description"></textarea>
+                </label>
+                <div className="buttonAddProduct">
+                  <button type="submit">Guardar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <ToastContainer position="bottom-center" autoClose={5000} />
         </div>
       </body>
-     </>
+    </>
   );
 };
 
