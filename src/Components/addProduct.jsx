9928e09d985c4ec4897342e76a42a489f82e6e40
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-06 15:35:13 -0500

    Capturar información formulario agregar productos

--- src/Components/addProduct.jsx
+++ src/Components/addProduct.jsx
@@ -7,64 +7,81 @@ const AddProduct = () => {
   // const [mostrarProductos,setMostrarProductos] = useState(false);
 
   const form = useRef(null);
+  const nuevoProducto = {};
 
   const submitForm = (e) => {
     e.preventDefault();
     const fd = new FormData(form.current);
 
-    const nuevoProducto = {};
     fd.forEach((value, key) => {
+      console.log(value, key);
       nuevoProducto[key] = value;
     });
     toast.success("Producto agregado con éxito");
   };
   return (
-      <>
+    <>
       <div className="AddProduct">
         <div className="BodyAddProduct">
-                <div>
-                <form ref={form} onSubmit={submitForm}>
-                    <div className="item1">
-                    <div className="IDContainer">
-                    <label className= "labelForm" htmlFor = "ID">
+          <div>
+            <form ref={form} onSubmit={submitForm}>
+              <div className="item1">
+                <div className="IDContainer">
+                  <label className="labelForm" htmlFor="ID">
                     ID
-                    <input className= "InputForm" required type="number"/>
-                    </label>
-                    </div>
-                    <div className="EstadoContainer">
-                    <label className= "labelForm" htmlFor = "Estado">
-                    Estado
-                    <select className= "InputForm" id="selectEstado" placeholder="seleccionar">
-                        <option>Disponible</option>
-                        <option>No Disponible</option>
-                    </select>
-                    </label>
-                    </div>
-                    </div>
-                    <div className="item3">
-                    <label className= "labelForm" htmlFor = "ValorUnitario">
-                    Valor unitario
-                    <input className= "InputForm" required type="number"/>
-                    </label>
-                    </div>
-                    <div className="item4">
-                    <label className= "labelForm" htmlFor = "Descripcion">
-                    Descripción
-                    <br/>
-                    <div>
-                  <textarea className="TArea" cols="75"></textarea>
-                    </div>
-                    </label>
-                    </div>
-                    <div className="BotonContainer">
-                   <button className="addProductButton" type="submit">Guardar</button>
-                    </div>
-                </form>
+                    <input
+                      name="id"
+                      className="InputForm"
+                      required
+                      type="number"
+                    />
+                  </label>
                 </div>
-                <ToastContainer position='bottom-center' autoClose={5000} />
-            </div>
+                <div className="EstadoContainer">
+                  <label className="labelForm" htmlFor="Estado">
+                    Estado
+                    <select
+                      className="InputForm"
+                      id="selection"
+                      name="selection"
+                    >
+                      <option>Disponible</option>
+                      <option>No Disponible</option>
+                    </select>
+                  </label>
+                </div>
+              </div>
+              <div className="item3">
+                <label className="labelForm" htmlFor="ValorUnitario">
+                  Valor unitario
+                  <input
+                    name="value"
+                    className="InputForm"
+                    required
+                    type="number"
+                  />
+                </label>
+              </div>
+              <div className="item4">
+                <label className="labelForm" htmlFor="Descripcion">
+                  Descripción
+                  <br />
+                  <div>
+                    <textarea className="TArea" cols="75"></textarea>
+                  </div>
+                </label>
+              </div>
+              <div className="BotonContainer">
+                <button className="addProductButton" type="submit">
+                  Guardar
+                </button>
+              </div>
+            </form>
+          </div>
+          <ToastContainer position="bottom-center" autoClose={5000} />
         </div>
-     </>
+      </div>
+    </>
   );
 };
 
