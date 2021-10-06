9b0a7bdacd480c8e478629400213566bf7600652
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-05 22:46:17 -0500

    capturar los datos de un select

--- src/Components/bodyProduct.jsx
+++ src/Components/bodyProduct.jsx
@@ -2,55 +2,72 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
-
-const Bodyproduct =()=>{
-  
-const productList =
-[
-{
-  id: "1",
-  name: "producto1",
-  value: 5900,
-},
-{
-  id: "2dc",
-  name: "producto2",
-  value: 10000,
-},
-{
-  id: "99f",
-  name: "Toyota",
-  value: 3000,
-},
-{
-  id: "40pc",
-  name: "Ford",
-  value: 8900,
-},
-{
-  id: "45-p",
-  name: "Mazda",
-  value: 4900,
-},
-{
-  id: "00-5c",
-  name: "Onix",
-  value: 7000,
-},
-];
+const Bodyproduct = () => {
+  const productList = [
+    {
+      id: "1",
+      name: "producto1",
+      value: 5900,
+      selection: "disponible",
+    },
+    {
+      id: "2dc",
+      name: "producto2",
+      value: 10000,
+      selection: "disponible",
+    },
+    {
+      id: "99f",
+      name: "producto3",
+      value: 3000,
+      selection: "disponible",
+    },
+    {
+      id: "40pc",
+      name: "producto4",
+      value: 8900,
+      selection: "disponible",
+    },
+    {
+      id: "45-p",
+      name: "producto5",
+      value: 4900,
+      selection: "disponible",
+    },
+    {
+      id: "00-5c",
+      name: "producto6",
+      value: 7000,
+      selection: "disponible",
+    },
+  ];
   const [products, setProducts] = useState([]);
 
-useEffect(() => {
-  setProducts(productList);
-}, []);
+  useEffect(() => {
+    setProducts(productList);
+  }, []);
 
-return(
-  <TableList productList={products}/>
-)
-}
+  return <TableList productList={products} />;
+};
 
-const FilaProducto = ({ product }) => {
+const ProductRow = ({ product }) => {
   const [edit, setEdit] = useState(false);
+  const [infoNewProduct, setInfoNewProduct] = useState({
+    name: product.name,
+    value: product.value,
+    selection: product.selection,
+  });
+
+  const updateProduct = () => {
+    console.log(infoNewProduct);
+  };
+
+  const getSelected = () => {
+    const combo = document.getElementById("list")
+    const selected = combo.options[combo.selectedIndex].text;
+    setInfoNewProduct({...infoNewProduct, selection:selected})
+    return(infoNewProduct)
+  }
 
   return (
     <tr class="fila_impar">
@@ -58,24 +75,38 @@ const FilaProducto = ({ product }) => {
         <>
           <td>{product.id}</td>
           <td>
-            <input className="editInput" type="text" />
+            <input
+              className="editInput"
+              type="text"
+              defaultValue={infoNewProduct.name}
+              onClick={(e) =>
+                setInfoNewProduct({ ...infoNewProduct, name: e.target.value })
+              }
+            />
           </td>
           <td>
-            <input className="editInput" type="number" />
+            <input
+              className="editInput"
+              type="number"
+              defaultValue={infoNewProduct.value}
+              onClick={(e) =>
+                setInfoNewProduct({ ...infoNewProduct, value: e.target.value })
+              }
+            />
           </td>
           <td>
             {" "}
             <div class="btn-group">
-              <select>
-                <option>Disponible</option>
-                <option>No Disponible</option>
+              <select id="list" name="list" onChange={()=>getSelected()}>
+                <option value="Disponible">Disponible</option>
+                <option value="No Disponible">No Disponible</option>
               </select>
               <div class="dropdown-menu"></div>
             </div>
           </td>
           <td>
             <div className="iconActions">
-              <button className="confirmButton">
+              <button onClick={() => updateProduct()} className="confirmButton">
                 <i
                   onClick={() => setEdit(!edit)}
                   class="bi bi-check-circle-fill"
@@ -97,9 +128,8 @@ const FilaProducto = ({ product }) => {
             <div class="btn-group">
               <select>
                 <option selected disabled>
-                  Disponible
+                  {product.selection}
                 </option>
-                <option disabled>No Disponible</option>
               </select>
               <div class="dropdown-menu"></div>
             </div>
@@ -141,7 +171,7 @@ const TableList = ({ productList }) => {
           </tr>
           <tbody>
             {productList.map((product) => {
-              return <FilaProducto key={nanoid} product={product} />;
+              return <ProductRow key={nanoid} product={product} />;
             })}
           </tbody>
         </table>
