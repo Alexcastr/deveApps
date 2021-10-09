d8b2bb20c2fc0b396078221c20e169544d0709c5
Cristian <krcamilo0417@gmail.com>
2021-10-09 16:33:41 -0500

    Botones para buscar en las páginas

--- src/Components/salesBody.jsx
+++ src/Components/salesBody.jsx
@@ -5,23 +5,92 @@ import Data from "../data.json";
 
 const SalesBody = () => {
   const [tableData, setTableData] = useState(Data);
-  const removeData = () => {
-    
-  }
+  const [filteringId, setFilteringId] = useState(false);
+  const [filteringClientId, setFilteringClientId] = useState(false);
+  const [filteringClient, setFilteringClient] = useState(false);
+
   return (
     <div className="salesTable">
       <div className="table">
         <table className="ml-auto mr-auto sales">
           <thead>
             <tr className="head-body">
-              <th>ID</th>
+              <th>
+                {!filteringId ? (
+                  <>
+                    ID
+                    <button
+                      onClick={() => {
+                        setFilteringId(true);
+                      }}
+                      className="tableSearch"
+                    >
+                      <i className="bi bi-search"></i>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <input type="number" min="1" placeholder="Id de la venta" />
+                    <button
+                      onClick={() => setFilteringId(false)}
+                      className="tableSearch"
+                    >
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </>
+                )}
+              </th>
               <th>Valor Total</th>
-              <th>ID Producto</th>
-              <th>Cantidad Producto</th>
-              <th>Precio Unitario</th>
+              <th>ID Productos</th>
+              <th>Cantidad Productos</th>
+              <th>Precios Unitarios</th>
               <th>Fecha de venta</th>
-              <th>ID Cliente</th>
-              <th>Cliente</th>
+              <th>
+                {!filteringClientId ? (
+                  <>
+                    ID Cliente
+                    <button
+                      className="tableSearch"
+                      onClick={() => setFilteringClientId(true)}
+                    >
+                      <i className="bi bi-search"></i>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <input type="number" min="1" placeholder="Id del cliente" />
+                    <button
+                      onClick={() => setFilteringClientId(false)}
+                      className="tableSearch"
+                    >
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </>
+                )}
+              </th>
+              <th>
+                {!filteringClient ? (
+                  <>
+                    Cliente
+                    <button
+                      className="tableSearch"
+                      onClick={() => setFilteringClient(true)}
+                    >
+                      <i className="bi bi-search"></i>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <input type="text" placeholder="Nombre del cliente" />
+                    <button
+                      onClick={() => setFilteringClient(false)}
+                      className="tableSearch"
+                    >
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </>
+                )}
+              </th>
               <th>Estado</th>
               <th>Vendedor</th>
               <th>Acciones</th>
@@ -36,9 +105,7 @@ const SalesBody = () => {
               </td>
             </tr>
             {tableData.map((item) => {
-              return (
-                <Sale key={item.ID} datos={item} setData={setTableData}/>
-              );
+              return <Sale key={item.ID} datos={item} setData={setTableData} />;
             })}
           </tbody>
         </table>
