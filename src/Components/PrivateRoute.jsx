332eb9b0717da192da8d5fc48f030aeae26191c9
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-15 10:27:05 -0500

    Agregar usuarios desde Auth0 a la BD

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import ReactLoading from "react-loading";
+import { getUserData } from "utils/apiUsers";
 
 const PrivateRoute = ({ children }) => {
   const {
@@ -13,23 +14,34 @@ const PrivateRoute = ({ children }) => {
   useEffect(() => {
     const fetchAuth0Token = async () => {
       const accessToken = await getAccessTokenSilently({
-        audience: 'api-autenticacion-deveapps',
-    });
-    localStorage.setItem('token', accessToken);
+        audience: "api-autenticacion-deveapps",
+      });
+      localStorage.setItem("token", accessToken);
+      await getUserData(
+        (response) => {
+          console.log("response", response);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
     };
-    if (isAuthenticated){
+    if (isAuthenticated) {
       fetchAuth0Token();
     }
-  },[isAuthenticated, getAccessTokenSilently]);
+  }, [isAuthenticated, getAccessTokenSilently]);
 
   if (isLoading) {
-    return <div><ReactLoading type="cylon" color="blue" height={667} width={375} /></div>;
+    return (
+      <div>
+        <ReactLoading type="cylon" color="blue" height={667} width={375} />
+      </div>
+    );
   }
   if (!isAuthenticated) {
     return loginWithRedirect();
   }
   return <>{children}</>;
-
 };
 
 export default PrivateRoute;
