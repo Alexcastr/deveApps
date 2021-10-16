2c36f9a358e3c7ce9d29ea9beb7bc4787a0a9255
Cristian <krcamilo0417@gmail.com>
2021-10-16 08:15:09 -0500

    Solucionados error de persmisos en añadir venta

--- src/Components/PrivateR.jsx
+++ src/Components/PrivateR.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { useUser } from "context/userContext";
 
 const PrivateR = ({ roleList, children }) => {
-    const {userData} = useUser();
-    console.log('userdata', userData);
-  if (roleList.includes(userData.role)) {
-    return children;
-  } 
-    return <div className='BannerDenegado'>No estás autorizado para ver este sitio.</div>;
+  const { userData } = useUser();
+  console.log("userdata", userData);
 
+  if (roleList.includes(userData.role)) {
+    return <div>{children}</div>;
+  } else {
+    return (
+      <div className="BannerDenegado">
+        No estás autorizado para ver este sitio.
+      </div>
+    );
+  }
 };
 
 export default PrivateR;
